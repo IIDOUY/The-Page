cd8ae0a54d4b21693b1ec66db71078fded439707
Khalid Ayaou <khalidayaou147@gmail.com>
2025-12-01 02:08:54 +0100

    Added HomePageSceen,CarteScreen,AllProductScreen and CartManager

---
+++ app/src/main/java/com/example/mypage/Data/CartManager.kt
@@ -0,0 +1,27 @@
+package com.example.mypage.Data
+
+import com.example.mypage.Data.Book
+
+object CartManager {
+    private val _cartItems = mutableListOf<Book>()
+    val cartItems: List<Book> get() = _cartItems.toList()
+
+    fun addToCart(book: Book) {
+
+        if (!_cartItems.any { it.id == book.id }) {
+            _cartItems.add(book)
+        }
+    }
+
+    fun removeFromCart(bookId: Int) {
+        _cartItems.removeAll { it.id == bookId }
+    }
+
+    fun getTotal(): Double {
+        return _cartItems.sumOf { it.price }
+    }
+
+    fun clearCart() {
+        _cartItems.clear()
+    }
+}
